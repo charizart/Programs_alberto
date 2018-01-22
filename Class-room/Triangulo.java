e0ef665c75bf22ec06387dea343e3ed102d99918
unknown <charizart>
2018-01-22 09:36:50 -0600

    hello

---
+++ Class-room/Triangulo.java
@@ -0,0 +1,18 @@
+/************
+alberto
+Triangulo.java
+the programs gives us the area of a triangle
+************/
+double b, h, area,real;
+public class Triangulo {
+     public static void main(String[] args) {
+        double b,h;
+        System.out.println("Ingresa base");
+        b=sc.nextDouble();
+        System.out.println("Ingresa altura");
+        h=sc.nextDouble();
+        double area;
+        area=b*h/2;
+       System.out.print(area);
+     }
+}
